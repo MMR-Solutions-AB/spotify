444a4e4a9f29497642091e2ec450eb1db4088232
Matt Bergstrom <matt@mmrsolutions.se>
2022-01-12 12:06:48 +0100

    stable version

--- src/components/ScreenRoot.js
+++ src/components/ScreenRoot.js
@@ -23,6 +23,8 @@ const setupSpotifyConnect = (token, setDeviceId)=>{
 	player.addListener('ready', ({ device_id }) => {
 		console.log('Ready with Device ID', device_id);
 		setDeviceId(device_id);
+
+		spotifyApi.transferMyPlayback([device_id]);
 	});
 
 	// Not Ready
@@ -89,7 +91,7 @@ const ScreenRoot = () => {
 								key={i}
 								path={route.path}
 								exact={route.exact}
-								render={(props) => <route.component {...props} />}
+								render={(props) => <route.component spotifyApi={spotifyApi} {...props} />}
 							/>
 						))}
 					</Switch>
--- src/components/SongRow/SongRow.js
+++ src/components/SongRow/SongRow.js
@@ -1,6 +1,13 @@
 import { TableCell, TableRow, Avatar, Typography, Skeleton, Box } from '@mui/material';
 
-const SongRow = ({ index, image, title, artist, album, duration, loading }) => {
+const SongRow = ({ spotifyApi, playlistId, track, index, loading }) => {
+
+	const image = track.album.images[2].url;
+	const title = track.name;
+	const artist = track.artists[0].name;
+	const album = track.album.name;
+	const duration = track.duration_ms / 1000;
+
 	const Title = () => {
 		return (
 			<Box sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
@@ -16,7 +23,7 @@ const SongRow = ({ index, image, title, artist, album, duration, loading }) => {
 	};
 
 	const formatTime = (value) => {
-		const rest = value % 60;
+		const rest = (value % 60).toFixed(0);
 		const min = Math.floor(value / 60);
 		const seconds = rest < 10 ? `0${rest}` : rest;
 		return `${min}:${seconds}`;
@@ -32,13 +39,18 @@ const SongRow = ({ index, image, title, artist, album, duration, loading }) => {
 					backgroundColor: '#d8d8d821 !important'
 				}
 			}}
-			onClick={() => console.log(`Play ${title}`)}
+			onClick={() => spotifyApi.play({
+				context_uri: `spotify:playlist:${playlistId}`,
+				offset: {
+					position: index
+				}
+			})}
 			hover={true}
 		>
 			<TableCell>{loading ? <Skeleton variant="rectangular" width={20} height={30} /> : index + 1}</TableCell>
 			<TableCell>{loading ? <Skeleton variant="rectangular" width={50} height={50} /> : <Title />}</TableCell>
 			<TableCell align="right" sx={{ color: 'text.secondary', display: { xs: 'none', md: 'table-cell' } }}>
-				{loading ? <Skeleton variant="rectangular" width={50} height={30} /> : album}
+				{loading ? <Skeleton variant="rectangular" width={50} height={30} /> : (album)}
 			</TableCell>
 			<TableCell align="right" sx={{ color: 'text.secondary', display: { xs: 'none', md: 'table-cell' } }}>
 				{loading ? <Skeleton variant="rectangular" width={20} height={30} /> : formatTime(duration)}
