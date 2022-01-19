ab10ecd8957ad255cebd6a8ecc8168f58779f252
marcuspousette <marcuspousette@gmail.com>
2022-01-19 09:11:21 +0100

    Revamp home screen

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import { ThemeProvider } from '@mui/system';
-import { mainTheme } from './style/material-themes';
+import mainTheme from './style/material-themes';
 import configureStore from './reduxStore/configureStore';
 import { Provider } from 'react-redux';
 
